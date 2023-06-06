ac61f8b1c05e617dedb8d888de148efa4dd90b4b
Mario Maia <mario.mtbr+github@outlook.com>
2023-06-06 02:11:04 -0300

    Remove ansible-doc because it is unmaintained

--- elisp/init-ansible.el
+++ elisp/init-ansible.el
@@ -2,25 +2,8 @@
 
 ;;; Commentary:
 ;;
-;;  TODO: check if ansible-doc has indeed been deprecated
 
 ;;; Code:
-(require 'package)
-(unless (package-installed-p 'ansible-doc)
-  (package-install 'ansible-doc))
-
-
-(with-eval-after-load 'yaml-mode
-  (require 'ansible-doc)
-  (defun cnr/ansible-doc-mode ()
-    "Enable `ansible-doc-mode' in properly named files."
-    (let ((file (file-name-nondirectory (buffer-file-name))))
-      (when (or (string= file "main.yml")
-                (string= file "playbook.yml"))
-        (ansible-doc-mode))))
-  (add-hook 'yaml-mode-hook #'cnr/ansible-doc-mode))
-
-
 (with-eval-after-load 'comint
   (let ((ansible-become-prompt-regexp "^BECOME password:[[:space:]]*\\'"))
     (unless (string-match-p (regexp-quote ansible-become-prompt-regexp)
