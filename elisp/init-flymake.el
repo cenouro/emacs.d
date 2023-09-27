adfc17146b1e7a43663d7cb6e33f75e5fe405bb0
Mario Maia <mario.mtbr+github@outlook.com>
2023-09-26 21:57:00 -0300

    Write down some TODOs

--- elisp/init-flymake.el
+++ elisp/init-flymake.el
@@ -1,7 +1,20 @@
 ;;; init-flymake.el --- Flymake settings  -*- lexical-binding: t; -*-
 
 ;;; Commentary:
-;;
+
+;; TODO: improve text-mode-hook: yaml, xml and probably other modes
+;;       inherit from text-mode instead of prog-mode. the hook logic
+;;       should not just enable languagetool everywhere.
+
+;; TODO: make languagetool ignore "errors" in git_commit comments.
+;;       this can probably be achieved by configuring
+;;       flymake-languagetool-ignore-faces-alist.
+
+;; TODO: check whether (require 'flymake-languagetool) is really
+;;       necessary or if we can rely on autoload.
+
+;; TODO: check whether (require 'flymake) is really necessary or if we
+;;       can rely on autoload.
 
 ;;; Code:
 (require 'flymake)
