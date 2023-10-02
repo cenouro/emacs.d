322257ef1589a0d0b7250dc3d62bd9836943009d
Mario Maia <mario.mtbr+github@outlook.com>
2023-10-02 17:35:23 -0300

    Make LanguageTool ignore comments when editing Magit commit messages
    I have tested these changes in "normal" commit messages, where the
    buffer is usually named COMMIT_EDITMSG. Other "types" of commit, such as
    when writing a custom merge commit message HAVE NOT BEEN TESTED.

--- elisp/init-flymake.el
+++ elisp/init-flymake.el
@@ -2,10 +2,6 @@
 
 ;;; Commentary:
 
-;; TODO: make languagetool ignore "errors" in git_commit comments.
-;;       this can probably be achieved by configuring
-;;       flymake-languagetool-ignore-faces-alist.
-
 ;; TODO: check whether (require 'flymake-languagetool) is really
 ;;       necessary or if we can rely on autoload.
 
@@ -37,8 +33,20 @@ derive from text-mode, such as yaml-mode and nxml-mode."
     (flymake-languagetool-load)
     (flymake-mode)))
 
+(defun cnr/git-commit/disable-languagetool-in-comments ()
+  (interactive)
+  (make-local-variable 'flymake-languagetool-ignore-faces-alist)
+  (customize-set-value 'flymake-languagetool-ignore-faces-alist
+                       '((text-mode font-lock-comment-face)))
+  ;; Start a syntax check so that changes to
+  ;; flymake-languagetool-ignore-faces-alist take effect.
+  (flymake-start '(post-command on-display)))
+
 (add-hook 'text-mode-hook #'cnr/text-mode/flymake-languagetool)
 
+(with-eval-after-load 'git-commit
+  (add-hook 'git-commit-setup-hook
+            #'cnr/git-commit/disable-languagetool-in-comments))
 
 (provide 'init-flymake)
 
