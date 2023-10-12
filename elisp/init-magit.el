14ace074716ff4f2511ba4c2615f5ab783b56251
Mario Maia <mario.mtbr+github@outlook.com>
2023-10-12 17:00:31 -0300

    Refactor 'git-commit-setup-hook
    (cherry picked from commit 6a1c87d2738e7a71ed39ac0b05997d5b6eb63b2c)

--- elisp/init-magit.el
+++ elisp/init-magit.el
@@ -2,6 +2,13 @@
 
 ;;; Commentary:
 ;;
+;; TODO: try to rely on autoloads instead of requiring magit and
+;;       magit-todos
+
+;; TODO: create a custom major mode and set `git-commit-major-mode'.
+;;       this will simplify the configuration of some variables that
+;;       are "major mode sensitive", such as
+;;       `flymake-languagetool-ignore-faces-alist'.
 
 ;;; Code:
 (require 'package)
@@ -14,10 +21,10 @@
 
 (require 'magit)
 
-(add-hook 'git-commit-setup-hook #'(lambda ()
-                                     (git-commit-turn-on-flyspell)
-                                     (git-commit-turn-on-auto-fill)
-                                     (customize-set-variable 'fill-column 72)))
+(add-hook 'git-commit-setup-hook #'git-commit-turn-on-auto-fill)
+(add-hook 'git-commit-setup-hook #'git-commit-turn-on-flyspell)
+(add-hook 'git-commit-setup-hook
+          (defun cnr/fill-column-72 () (setq fill-column 72)))
 
 
 (require 'magit-todos)
