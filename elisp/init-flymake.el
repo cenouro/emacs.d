5780c356a6a28b59f079ff4d68c3b6300151982f
Mario Maia <mario.mtbr+github@outlook.com>
2023-11-04 11:55:06 -0300

    Update code that disables languagetool in git commit comments
    A custom function that creates a buffer local variable, has to use
    customize-set-value instead of customize-set-variable to set its value
    locally and then triggers a syntax check so that changes take effect is
    no longer necessary, which is a huge improvement.

--- elisp/init-flymake.el
+++ elisp/init-flymake.el
@@ -22,27 +22,21 @@
                                 "/LanguageTool/languagetool-server.jar"))
 
 (with-eval-after-load 'init-magit
+  (customize-set-variable
+   'flymake-languagetool-ignore-faces-alist
+   (add-to-list 'flymake-languagetool-ignore-faces-alist
+                '(cnr/git-commit-mode font-lock-comment-face
+                                      git-commit-comment-action
+                                      git-commit-comment-branch-local
+                                      git-commit-comment-branch-remote
+                                      git-commit-comment-detached
+                                      git-commit-comment-file
+                                      git-commit-comment-heading))
+   "Disable LanguageTool in Git commit comments.")
+
   (add-hook 'cnr/git-commit-mode-hook #'flymake-languagetool-load)
   (add-hook 'cnr/git-commit-mode-hook #'flymake-mode 90))
 
-(defun cnr/git-commit/disable-languagetool-in-comments ()
-  (interactive)
-  (make-local-variable 'flymake-languagetool-ignore-faces-alist)
-  (customize-set-value 'flymake-languagetool-ignore-faces-alist
-                       '((text-mode font-lock-comment-face
-                                    git-commit-comment-action
-                                    git-commit-comment-branch-local
-                                    git-commit-comment-branch-remote
-                                    git-commit-comment-detached
-                                    git-commit-comment-file
-                                    git-commit-comment-heading)))
-  ;; Start a syntax check so that changes to
-  ;; flymake-languagetool-ignore-faces-alist take effect.
-  (flymake-start '(post-command on-display)))
-
-(with-eval-after-load 'git-commit
-  (add-hook 'git-commit-setup-hook
-            #'cnr/git-commit/disable-languagetool-in-comments))
 
 (provide 'init-flymake)
 
