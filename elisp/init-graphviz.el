6634e25496942898de9ede313becce59b4025a8b
Mario Maia <mario.mtbr+github@outlook.com>
2024-02-06 10:19:51 -0300

    Work around graphviz-dot-mode buggy PNG previews

--- elisp/init-graphviz.el
+++ elisp/init-graphviz.el
@@ -3,12 +3,18 @@
 ;;; Commentary:
 
 ;;; Code:
+(require 'init-custom)
 (require 'init-package)
 (require 'init-prog)
 
 (unless (package-installed-p 'graphviz-dot-mode)
   (package-vc-install "https://github.com/ppareit/graphviz-dot-mode"))
 (require 'graphviz-dot-mode)
+
+(my/customize-set-variable "Ad hoc fix for preview glitch."
+                           "See ppareit/graphviz-dot-mode/issues/75"
+                           "for details."
+                           'graphviz-dot-preview-extension "svg")
 
 (provide 'init-graphviz)
 ;;; init-graphviz.el ends here
