b8911c24285a0fab9f339f1e0ae3310278f41282
Mario Maia <mario.mtbr+github@outlook.com>
2024-02-09 17:40:46 -0300

    Customize helm size and disable cycling

--- elisp/init-helm.el
+++ elisp/init-helm.el
@@ -3,6 +3,7 @@
 ;;; Commentary:
 
 ;;; Code:
+(require 'init-custom)
 (require 'init-package)
 (unless (package-installed-p 'helm)
   (package-install 'helm))
@@ -12,9 +13,18 @@
                         "Required for TAB completion")
 
 (customize-set-variable 'helm-command-prefix-key nil)
-
 (global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
+
+(my/customize-set-variable "This disables cycling but"
+                           "allows C-n/p to move through sources"
+                           'helm-move-to-line-cycle-in-source nil)
+
+(customize-set-variable 'helm-always-two-windows nil)
+(customize-set-variable 'helm-default-display-buffer-functions
+                        '(display-buffer-at-bottom))
+(my/customize-set-variable "This is in number of lines"
+                           'helm-display-buffer-default-height 13)
 
 (helm-mode 1)
 (with-eval-after-load 'diminish
