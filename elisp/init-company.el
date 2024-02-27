acbf1c468f11623b886ce4d5334c28088925f2b8
Mario Maia <mario.mtbr+github@outlook.com>
2024-02-27 17:09:53 -0300

    Customize company

--- elisp/init-company.el
+++ elisp/init-company.el
@@ -11,8 +11,14 @@
 
 (global-set-key (kbd "C-i") #'company-indent-or-complete-common)
 
+(customize-set-variable 'company-dabbrev-code-ignore-case t)
 (customize-set-variable 'company-format-margin-function #'company-text-icons-margin)
+(customize-set-variable 'company-minimum-prefix-length 5)
 (customize-set-variable 'company-search-regexp-function #'company-search-words-in-any-order-regexp)
+(customize-set-variable 'company-tooltip-align-annotations t)
+(customize-set-variable 'company-tooltip-annotation-padding 4)
+(customize-set-variable 'company-tooltip-minimum-width 16)
+(customize-set-variable 'company-tooltip-width-grow-only t)
 
 (add-hook 'after-init-hook #'global-company-mode)
 
